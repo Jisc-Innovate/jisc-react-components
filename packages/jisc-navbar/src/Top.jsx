16d7bbd34aa9d71af4b572648880d812f851eea9
Edoardo L'Astorina <edoardo@blufra.me>
2017-03-16 22:40:01 +0000

    Top component split in Default and Focussed

---
+++ packages/jisc-navbar/src/Top.jsx
@@ -0,0 +1,37 @@
+import React, { PropTypes } from "react"
+import { Link } from "react-router"
+import image from "url-loader!./logo.png"
+
+import TopDefault from "./TopDefault.jsx"
+import TopFocussed from "./TopFocussed.jsx"
+
+class Top extends React.Component {
+  render( ) {
+    const { focussed, focussedTitle, focussedItems } = this.props
+
+    return(
+      <div className="masthead__top">
+        <div className="inner">
+          <Link className="masthead__logo" to="/">
+            <img src={image} alt="Jisc logo" />
+          </Link>
+
+          { focussed ?
+            <TopFocussed
+              title={focussedTitle}
+              items={focussedItems} />
+            :
+            <TopDefault />
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+Top.propTypes = {
+  focussed: PropTypes.bool,
+  focussedTitle: PropTypes.string
+}
+
+export default Top
---
+++ packages/jisc-navbar/src/TopFocussed.jsx
@@ -0,0 +1,60 @@
+import React, { PropTypes } from "react"
+
+class TopFocussed extends React.Component {
+  renderItems( ) {
+    const { items } = this.props
+
+    items
+      .map( ( item, i ) => {
+        const { link, name } = this.props
+
+        return(
+          <li
+            key={i}
+            className="masthead__nav__item">
+              <a href={`${link}`}>
+                {name}
+              </a>
+          </li>
+        )
+      })
+  }
+
+  render( ) {
+    const { title } = this.props
+
+    return(
+      <div>
+        <div id="nav" className="title-nav" data-dropdown="">
+          <div className="has-popup masthead__nav__item" data-dropdown-item="">
+            <h1 className="masthead__title masthead__title--long">
+              <a href="/">
+                {title}
+              </a>
+            </h1>
+          </div>
+        </div>
+
+        <nav
+          className="masthead__nav masthead__nav--primary"
+          role="navigation"
+          data-dropdown>
+            <ul>
+              {this.renderItems( )}
+            </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+TopFocussed.propTypes = {
+  title: PropTypes.string,
+
+  items: PropTypes.arrayOf(PropTypes.shape({
+    link: PropTypes.string,
+    name: PropTypes.string.isRequired
+  }).isRequired)
+}
+
+export default TopFocussed
